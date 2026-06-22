f5bd76dc3e5db016dda06779c3eed154cead4c77
rajesh-patil1 <115767329+rajesh-patil1@users.noreply.github.com>
2026-06-22 17:48:30 +0530

    Update App.java
    11

--- App.java
+++ App.java
@@ -1,7 +1,13 @@
-package com.example;
+package com.demo;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Docker Pipeline");
+
+        System.out.println("===================================");
+        System.out.println("Hello from Jenkins CI/CD Pipeline");
+        System.out.println("Java Version: 21");
+        System.out.println("Docker Image Build Successful");
+        System.out.println("===================================");
+
     }
 }
