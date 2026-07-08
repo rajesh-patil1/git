53ec37d65b5e649eacd04185a52ad1c69492313b
rajesh-patil1 <115767329+rajesh-patil1@users.noreply.github.com>
2026-07-09 00:09:43 +0530

    Update App.java
    ....

--- App.java
+++ App.java
@@ -4,9 +4,9 @@ public class App {
     public static void main(String[] args) {
 
         System.out.println("===================================");
-        System.out.println("Hello from Jenkins CI/CD Pipeline");
-        System.out.println("Java Version: 21");
-        System.out.println("Docker Image Build Successful");
+        System.out.println("Hello from Jenkins CI/CD Pipeline............");
+        System.out.println("Java Version: 21............");
+        System.out.println("Docker Image Build Successful..........");
         System.out.println("===================================");
 
     }
